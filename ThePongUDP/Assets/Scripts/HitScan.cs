3730dd9b5c57bdc1ad6215714939883ef0b92cd7
maddy <luis.maia@escolar.ifrn.edu.br>
2025-10-13 16:51:03 -0300

    ThePongUDP 13/10/2025

--- ThePongUDP/Assets/Scripts/HitScan.cs
+++ ThePongUDP/Assets/Scripts/HitScan.cs
@@ -1,31 +1,34 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class HitScan : MonoBehaviour
 {
     public GameObject Game;
     public GameManager gameManager;
+    private PongClientUDP networkClient; // referência ao script de rede
+
     private void Start()
-    { 
+    {
         gameManager = Game.GetComponent<GameManager>();
+        networkClient = FindAnyObjectByType<PongClientUDP>();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void OnCollisionEnter2D(Collision2D collision) //score P1
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Map Limit Left")
+        // Só o player 1 (dono da bola) decide o gol
+        if (networkClient == null || networkClient.myId != 1)
+            return;
+
+        if (collision.gameObject.CompareTag("Map Limit Left"))
         {
             gameManager.Player2Scored();
-
+            networkClient.SendGoalScored(2);
+            networkClient.SendReset();
         }
-        
-        if (collision.gameObject.tag == "Map Limit Right")
+        else if (collision.gameObject.CompareTag("Map Limit Right"))
         {
             gameManager.Player1Scored();
+            networkClient.SendGoalScored(1);
+            networkClient.SendReset();
         }
-        
     }
-    
-    
 }
